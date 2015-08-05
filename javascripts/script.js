a7341adca7a4cfeefa7781b729d3231845edc31d
Colin Mahaffey <mahaffeyc@Colins-MacBook-Air.local>
2015-08-05 13:59:47 -0400

    init commit

---
+++ javascripts/script.js
@@ -0,0 +1,29 @@
+console.log('let\'s play');
+//set up a board
+function Board(size){
+  this.box=[];
+  for (var i=0;i<size;i++){
+    this.box.push([]);
+    for(var j=0;j<size;j++){
+      this.box[i].push([]);
+    }
+  }
+  return this.box;
+}
+//get an input
+
+//get a win
+Board.prototype.getWin=function(){
+  for (var i=0;)
+}
+//play
+
+//choosing x or o
+
+//1 or 2 player
+
+//set name
+
+//comp ai
+
+//big boards
