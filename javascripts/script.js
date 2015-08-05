91e253bb94811fa7fbd21745de3d88f42a166d7f
Colin Mahaffey <mahaffeyc@Colins-MacBook-Air.local>
2015-08-05 15:02:25 -0400

    adds a constructor for the board

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,6 +1,15 @@
 console.log('let\'s play');
 //set up a board
-
+function Board(size){
+  this.box=[];
+  for (var i=0;i<size;i++){
+    this.box.push([]);
+    for(var j=0;j<size;j++){
+      this.box[i].push([]);
+    }
+  }
+  return this.box;
+}
 //get an input
 
 //get a win
